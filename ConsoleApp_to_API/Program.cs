cf6bfd7a26bf5798cfa98a7b51c0ece1e4d6c591
Ákos Klobetz <klobetz@gmail.com>
2024-09-24 16:30:18 +0200

    #api DB elérés Rázsó

---
+++ ConsoleApp_to_API/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsolaApp_To_API.ViewModel;
+using System.Net.Http.Json;
+
+Console.WriteLine("Api DB-adatok kiíratása");
+HttpClient client = new HttpClient();
+
+var valasz = await client.GetFromJsonAsync<List<UserVM>>("https://localhost:7132/api/User");
+
+if (valasz != null)
+{
+    foreach (var user in valasz)
+    {
+        Console.WriteLine($"A felhasztnáló neve: {user.FullName} \nE-Mail címe: {user.Email}\n");
+    }
+}
+else
+{
+    Console.WriteLine("Nincs adat!");
+}
+
+Console.ReadLine();
---
+++ ConsoleApp_to_API/ViewModel/UserVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsolaApp_To_API.ViewModel
+
+{
+    public class UserVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LName { get; set; }= string.Empty;
+        
+        [Required]
+        [MaxLength(100)]
+        [RegularExpression(@"^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W]{1,})$")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$")]
+        public string Password { get; set; } = string.Empty;
+
+        public string FullName => $"{FName} {LName}";
+
+    }
+}
