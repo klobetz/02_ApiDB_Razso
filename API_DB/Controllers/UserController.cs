4dc413a56d86eb9e3f325bcc05dd14574f6db9d7
Ákos Klobetz <klobetz@gmail.com>
2024-09-24 19:01:53 +0200

    #felület + Api (Post)

--- API_DB/Controllers/UserController.cs
+++ API_DB/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using API_DB.DAL.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mysqlx;
 
 namespace API_DB.Controllers
 {
@@ -16,11 +18,36 @@ namespace API_DB.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpGet]
+        [HttpGet] //lekérem a DB-ből az öszes adatot és egy listába beleteszem
         public async Task<ActionResult<List<User>>> GetAllUser()
         {
             var users = _dbContext.users.ToList();
             return Ok(users);
         }
+        //egy bizonyos ID alapján kiszűröm a user-t
+        [HttpGet]
+        [Route("{id}")] //[HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserName(int id)
+        {
+            var userSearch = await _dbContext.users.FindAsync(id);
+            if (userSearch == null)
+            {
+                return NotFound("the user is not found");
+            }
+            return Ok(userSearch);
+        }
+
+        //a felhasználó létrehozás
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            _dbContext.users.Add(user);
+            await _dbContext.SaveChangesAsync(); //mentem a DB-be az adatokat
+            return Ok(await _dbContext.users.ToListAsync()); //ezzel a kapott eredményt látni fogom!! egyből
+        }
+
+
+
+
     }
 }
